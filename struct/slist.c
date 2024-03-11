874b6ba33fdc99205c27cc38787bd9447479cdca
Jeff <jeffreymanzione@gmail.com>
2024-03-10 22:47:54 -0700

    Start impl of slist

--- struct/slist.c
+++ struct/slist.c
@@ -1,26 +1,29 @@
 #include "struct/slist.h"
 
-IMPL_ARRAYLIKE(_BlockList, _Block);
-IMPL_ARRAYLIKE(_VectorList, _Vector);
+#include "alloc/alloc.h"
+
+#define SLIST_BLOCK_SIZE 32
 
 void slist_init(SList *sl, size_t member_size) {
-  _VectorList_init(&sl->_vectors);
-  sl->_start = NULL;
-  sl->_end = NULL;
+  __alist_init(&sl->_blocks, ALLOC_ARRAY(char, member_size *SLIST_BLOCK_SIZE),
+               member_size, SLIST_BLOCK_SIZE);
   sl->_member_size = member_size;
   sl->_size = 0;
 }
 
-void slist_finalize(SList *sl) { _VectorList_finalize(&sl->_vectors); }
+void slist_finalize(SList *sl) { alist_finalize(&sl->_blocks); }
 
-size_t slist_len(const SList *const sl) {}
+size_t slist_len(const SList *const sl) { return sl->_size; }
 
 void *slist_get(SList *const sl, uint32_t index) {}
 
-void *slist_add_last(SList *const sl) { sl->_size++; }
+void *slist_add_last(SList *const sl) {
+  if (sl->_size == alist_len(&sl->_blocks) * SLIST_BLOCK_SIZE) {
+    *(char *)alist_add(&sl->_blocks) =
+        ALLOC_ARRAY(char, sl->_member_size *SLIST_BLOCK_SIZE);
+  }
+  char *last = alist_get(&sl->_blocks, alist_len(&sl->_blocks) - 1);
+  sl->_size++;
+}
 
 void slist_remove_last(SList *const sl) { sl->_size--; }
-
-void slist_remove_at(SList *const sl, uint32_t index) { sl->_size--; }
-
-void *slist_insert(SList *const sl, uint32_t index) { sl->_size++; }
--- struct/slist.h
+++ struct/slist.h
@@ -3,30 +3,12 @@
 
 #include <stdint.h>
 
-#include "struct/arraylike.h"
+#include "struct/alist.h"
 
 typedef struct {
-  void *_start;
-  uint32_t _length;
-  size_t _max_length;
-} _Block;
-
-DEFINE_ARRAYLIKE(_BlockList, _Block);
-
-typedef struct {
-  void *_arr;
-  _BlockList _blocks;
-} _Vector;
-
-DEFINE_ARRAYLIKE(_VectorList, _Vector);
-
-typedef struct {
-  _VectorList _vectors;
-  void *_start;
-  void *_end;
   size_t _member_size;
   size_t _size;
-
+  AList blocks;
 } SList;
 
 void slist_init(SList *sl, size_t member_size);
@@ -35,7 +17,5 @@ size_t slist_len(const SList *const sl);
 void *slist_get(SList *const sl, uint32_t index);
 void *slist_add_last(SList *const sl);
 void slist_remove_last(SList *const sl);
-void slist_remove_at(SList *const sl, uint32_t index);
-void *slist_insert(SList *const sl, uint32_t index);
 
 #endif /* STRUCT_SLIST_H_ */
